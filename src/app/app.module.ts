cff1094d665af6e66cb54df05e4b977adcc7ca5a
Ramprits <rampritsahani@outlook.in>
2019-07-15 15:06:01 +0530

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './shared/loading/loading.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, LoadingComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
